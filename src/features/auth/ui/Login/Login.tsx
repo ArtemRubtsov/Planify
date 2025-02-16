f536e0ac23ac8eca47e893fa1b46972501be6703
Artem Rubtsov <ua.rubtsov.a@gmail.com>
2025-02-16 15:13:31 +0200

    feat: hashRouter - react router

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -51,9 +51,9 @@ export const Login = () => {
   if (isLoggedIn) {
     return <Navigate to={"/"} />
   }
-  if(isLoggedIn){
-    return <Navigate to={"/login"} />
-  }
+  // if(isLoggedIn){
+  //   return <Navigate to={"/login"} />
+  // }
 
   return (
     <Grid container justifyContent={"center"}>
